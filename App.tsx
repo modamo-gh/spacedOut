5e5e3e3e5cfe655ec2e92a7dbd888aa50bbed223
modamo-gh <morganthemosaic@gmail.com>
2024-12-11 21:17:11 -0500

    Set up visibility props for DatePicker

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { TextInput, View } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const App = () => {
-	return <View style={{ flex: 1, backgroundColor: "blue", alignItems: "center", justifyContent: "center" }}>
-		<DateTimePickerModal 
-		isVisible="true"
-		mode="date"/>
-	</View>
-}
+	const [showDatePicker, setShowDatePicker] = useState(true);
+
+	const handleConfirm = (date: Date) => {
+		console.log(date);
+		
+		setShowDatePicker(false)
+	};
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: "blue",
+				alignItems: "center",
+				justifyContent: "center"
+			}}
+		>
+			<DateTimePickerModal
+				isVisible={showDatePicker}
+				mode="date"
+				onCancel={() => setShowDatePicker(false)}
+				onConfirm={handleConfirm}
+			/>
+		</View>
+	);
+};
 
 export default App;
