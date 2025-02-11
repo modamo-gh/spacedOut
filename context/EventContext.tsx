b1df200d37da77935b8e614f28ffe613273e04ae
modamo-gh <morganthemosaic@gmail.com>
2025-02-11 10:24:42 -0500

    Ensure useEventContext always returns valid context by throwing error if undefined

--- context/EventContext.tsx
+++ context/EventContext.tsx
@@ -65,5 +65,8 @@ export const EventProvider: React.FC<{ children: React.ReactNode }> = ({
 export const useEventContext = () => {
 	const context = useContext(EventContext);
 
+	if (!context) {
+		throw new Error("useEventContext must be used within an EventProvider");
+	}
 	return context;
 };
